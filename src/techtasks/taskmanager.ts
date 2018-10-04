cd0c6c9a401a8f5e8600da173e7ffad11b33e8e1
Sairama Jamalapuram <sairamaj@hotmail.com>
2018-10-04 23:50:08 +0000

    added git authentication

--- src/techtasks/taskmanager.ts
+++ src/techtasks/taskmanager.ts
@@ -2,11 +2,17 @@ import { Task } from "./task";
 const octokit = require('@octokit/rest')()
 const Base64 = require('js-base64').Base64;
 
-export class TaskManager{
+export class TaskManager {
 
-
-    async getRepoFileContent(file) : Promise<string>{
+    accessToken: string = " a3b84d9bf651459b22be0c1f12cf8f56188d6fd2";
+    getAuthHeader(): any {
+        return {
+            authorization: `Bearer ${this.accessToken}`
+        }
+    }
+    async getRepoFileContent(file): Promise<string> {
         var response = await octokit.repos.getContent({
+            headers: this.getAuthHeader(),
             owner: 'sairamaj',
             repo: 'techtips',
             path: file
@@ -14,21 +20,22 @@ export class TaskManager{
         return Base64.decode(response.data.content)
     }
 
-    async getRepoFiles(path) : Promise<Array<string>> {
+    async getRepoFiles(path): Promise<Array<string>> {
         var info = await octokit.repos.getContent({
+            headers: this.getAuthHeader(),
             owner: 'sairamaj',
             repo: 'techtips',
             path: path
         })
-        return info.data.map(p=> p.name);
+        return info.data.map(p => p.name);
     }
 
-    async getTasks() : Promise<Array<Task>>{
-         var mdFiles = await  this.getRepoFiles('data/techtasks/azure')
-         return mdFiles.map(mdFile=> new Task(mdFile))
+    async getTasks(): Promise<Array<Task>> {
+        var mdFiles = await this.getRepoFiles('data/techtasks/azure')
+        return mdFiles.map(mdFile => new Task(mdFile))
     }
 
-    async getTaskDetail(name:string) : Promise<Task>{
+    async getTaskDetail(name: string): Promise<Task> {
         var content = await this.getRepoFileContent(`data/techtasks/azure/${name}`);
         return new Task(name, content);
     }
