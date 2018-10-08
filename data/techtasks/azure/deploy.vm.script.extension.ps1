317c676b1a64a358131b8156cf1fece9827c2764
sairamaj <sairamaj@gmail.com>
2018-10-07 18:58:13 -0700

    custom script extension working

--- data/techtasks/azure/deploy.vm.script.extension.ps1
+++ data/techtasks/azure/deploy.vm.script.extension.ps1
@@ -3,10 +3,18 @@ Select-AzureRmSubscription -SubscriptionName 'Visual Studio Enterprise'
 $ResourceGroupName = 'testing'
 $TemplateFile = 'templates\vm.script.extension.json'
 $TemplateParameterFile = 'templates\vm.script.extension.parameters.json'
-
+$DeploymentName = 'TestAutomationDeployment'
 # Creating a deployment 
+
 New-AzureRmResourceGroupDeployment `
-    -Name TestAutomationDeployment `
+    -Name $DeploymentName `
     -ResourceGroupName $ResourceGroupName `
     -TemplateFile $TemplateFile `
-    -TemplateParameterFile $TemplateParameterFile
+    -TemplateParameterFile $TemplateParameterFile `
+    -AsJob
+
+Write-Host "Deployment has been submitted."
+Write-Host "Wait 30 seconds before querying the status."
+Start-Sleep -Seconds 30
+Write-Host "Getting Deployment status"
+Get-AzureRmResourceGroupDeployment -ResourceGroupName $ResourceGroupName -Name $DeploymentName
