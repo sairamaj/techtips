c850f21e351af95b27443def9356307656a496a3
sairamaj <sairamaj@gmail.com>
2019-04-12 10:38:38 -0700

    fixed the location in service template

--- deployment/New-Deployment.ps1
+++ deployment/New-Deployment.ps1
@@ -1,6 +1,6 @@
 # https://raw.githubusercontent.com/sairamaj/techtips/master/deployment/armtemplates
 <# Test the arm templates #>
-Test-AzureRmResourceGroupDeployment -ResourceGroupName deleteme `
+New-AzureRmResourceGroupDeployment -ResourceGroupName deleteme `
  -TemplateFile .\armtemplates\master.template.json `
  -TemplateParameterFile .\armtemplates\master.parameters.json `
- -repoBaseUrl https://raw.githubusercontent.com/sairamaj/techtips/master/deployment/armtemplates
+  -repoBaseUrl https://raw.githubusercontent.com/sairamaj/techtips/master/deployment/armtemplates
