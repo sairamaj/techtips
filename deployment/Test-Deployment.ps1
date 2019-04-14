4e997c7cdb94f48438e8137abf1abceff5b0b013
sairamaj <sairamaj@gmail.com>
2019-04-14 10:25:23 -0700

    Splitting resources

--- deployment/Test-Deployment.ps1
+++ deployment/Test-Deployment.ps1
@@ -1,6 +1,9 @@
 # https://raw.githubusercontent.com/sairamaj/techtips/master/deployment/armtemplates
 <# Test the arm templates #>
-Test-AzureRmResourceGroupDeployment -ResourceGroupName deletemeapim `
+$results = `Test-AzureRmResourceGroupDeployment -ResourceGroupName deletemeapim `
  -TemplateFile .\armtemplates\master.template.json `
  -TemplateParameterFile .\armtemplates\master.parameters.json `
  -repoBaseUrl https://raw.githubusercontent.com/sairamaj/techtips/master/deployment/armtemplates
+
+ $results |fl
+ $results.Details |fl
