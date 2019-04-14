7be0b9dd9fe88e36b7c01433d69ecdcd6d99674b
sairamaj <sairamaj@gmail.com>
2019-04-14 13:07:54 -0700

    added README.MD

--- bareMinimumDeployment/New-Deployment.ps1
+++ bareMinimumDeployment/New-Deployment.ps1
@@ -1,6 +1,6 @@
 # https://raw.githubusercontent.com/sairamaj/techtips/master/deployment/armtemplates
 <# Test the arm templates #>
 New-AzureRmResourceGroupDeployment -ResourceGroupName deletemeapim `
- -TemplateFile .\armtemplates\master.template.full.json `
+ -TemplateFile .\armtemplates\master.template.json `
  -TemplateParameterFile .\armtemplates\master.parameters.json `
   -repoBaseUrl https://raw.githubusercontent.com/sairamaj/techtips/master/deployment/armtemplates
